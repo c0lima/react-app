70f9221e35996f0eb47a24404ed193e01770909c
c0lima <colima.msc@gmail.com>
2023-03-15 13:34:11 -0300

    fix: fix github token

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-        "repositoryUrl": "https://github.com/c0lima/react-app/",
+        "repositoryUrl": "https://github.com/c0lima/react-app",
         "branches": ["main"],
         "plugins": ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/npm","@semantic-release/github"]
 }
