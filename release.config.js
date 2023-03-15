e90e7c192a8b6e5ae6c64e22668428473e006d57
c0lima <colima.msc@gmail.com>
2023-03-15 13:08:46 -0300

    fix: move semantic variables to package.json

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
         "repositoryUrl": "https://github.com/c0lima/react-app",
-         "branches": ["main"],
-         "plugins": ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/npm","@semantic-release/github"]
+        "branches": "main",
+        "plugins": ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/npm","@semantic-release/github"]
 }
