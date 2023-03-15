298e4c26f2ea952c45314718619cebbcc54c0501
c0lima <colima.msc@gmail.com>
2023-03-15 11:46:21 -0300

    fix: typo

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    branches: "master",
+    branches: ["main"],
     repositoryUrl: "https://github.com/c0lima/react-app",
     plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/npm","@semantic-release/github"]
 
