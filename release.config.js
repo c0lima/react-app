9ee825f09a25c7aa8d1fd20f63ca5715ee1293b3
c0lima <colima.msc@gmail.com>
2023-03-15 12:33:15 -0300

    fix: update semantic variable to access github ci

--- release.config.js
+++ release.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 
     branches: ["main"],
-    repository: "https://github.com/c0lima/react-app",
     plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/npm","@semantic-release/github"]
 }
 
