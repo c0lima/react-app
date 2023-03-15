35f2806ad19811d068f24768c891292de93bb187
c0lima <colima.msc@gmail.com>
2023-03-15 13:52:58 -0300

    fix: compress assets

--- release.config.js
+++ release.config.js
@@ -2,5 +2,15 @@ module.exports = {
 
         "repositoryUrl": "https://github.com/c0lima/react-app",
         "branches": ["main"],
-        "plugins": ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/npm","@semantic-release/github"]
+        "plugins": [
+        "@semantic-release/commit-analyzer", 
+        "@semantic-release/release-notes-generator", 
+        "@semantic-release/npm",
+        ["@semantic-release/github",{
+            "assets": [
+                { path: "build.zip", label: "Build" },
+                { path: "coverage.zip", label: "Coverage" }
+            ]
+        }]
+        ]
 }
