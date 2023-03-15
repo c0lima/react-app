3db088f880994d15f7ad331f6bc261eedd0d6b15
c0lima <colima.msc@gmail.com>
2023-03-15 12:23:44 -0300

    fix: add github token ref

--- release.config.js
+++ release.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    
     branches: ["main"],
     repositoryUrl: "https://github.com/c0lima/react-app",
     plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/npm","@semantic-release/github"]
