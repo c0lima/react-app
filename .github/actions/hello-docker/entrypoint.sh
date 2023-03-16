8f78e2d6ef508c62f92d488c1aa21c07483b27a2
c0lima <colima.msc@gmail.com>
2023-03-16 16:41:48 -0300

    feat(actions.yml): add docker-hello action

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -l
+
+echo "::debug:: Debug message"
+echo "::warning:: Warning message"
+echo "::error:: error message"
+
+echo "::add-mask::$1"
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
+
+echo "::group::Some Expandable logs"
+echo "some stuff"
+echo "some stuff"
+echo "some stuff"
+echo "::endgroup::"
+
+echo "::set-env name=HELLO::hello"
